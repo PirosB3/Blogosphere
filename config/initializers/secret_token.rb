76057fd2e14a9168ee13e596b3c3d77364ea6f00
Mathilda Thompson <mathildathompson288@gmail.com>
2013-08-17 21:59:26 +0100

    Added devise and Twitter Bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blogosphere::Application.config.secret_token = '5db82204ece604af831b3aec7201782ada656ab09a84b357d98f3c9179340471b633cdaf956dcbf174b9d6056bf321c96c9625f6e8aeef3f0f134143540a8b7b'
