c69e6d1cf438f8e11b69ce3ad4877f82b14f2e00
Daniel Pyrathon <pirosb3@gmail.com>
2013-09-28 01:28:33 +0100

    patched small issue with validation

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,55 +1,56 @@
 class CheckoutController < ApplicationController
-  before_filter :authenticate_user!, :only => [:create]
+	before_filter :authenticate_user!, :only => [:create]
 
-  def get_purchase_information
-    @to_purchase_magazines = get_cart_magazines
-    @total_price_magazines = get_cart_price
+	def get_purchase_information
+		@to_purchase_magazines = get_cart_magazines
+		@total_price_magazines = get_cart_price
 
-    @has_at_least_a_post = @to_purchase_magazines.any? do |magazine|
-      magazine.purchase_type == 'post'
-    end
-    @packaging_price = @has_at_least_a_post ? 5000 : 0
+		@has_at_least_a_post = @to_purchase_magazines.any? do |magazine|
+			magazine.purchase_type == 'post'
+		end
+		@packaging_price = @has_at_least_a_post ? 5000 : 0
 
-    @subtotal = @total_price_magazines + @packaging_price
-  end
+		@subtotal = @total_price_magazines + @packaging_price
+	end
 
 	def new
-    get_purchase_information
-    @checkout = Checkout.new
-  end
+		get_purchase_information
+		@checkout = Checkout.new
+	end
 
 	def create
-    @checkout = Checkout.new(params[:checkout])
-    get_purchase_information
-    unless @checkout.valid?
-      return render 'new'
-    end
+		get_purchase_information
+		@checkout = Checkout.new(params[:checkout])
+		@checkout.is_by_post = @has_at_least_a_post
+		unless @checkout.valid?
+			return render 'new'
+		end
 
 		# Create a Customer
 		customer = Stripe::Customer.create(
-		  :card => params[:stripeToken],
-		  :description => current_user.email
+			:card => params[:stripeToken],
+			:description => current_user.email
 		)
 
 		# Charge the Customer instead of the card
 		charge = Stripe::Charge.create(
-		    :amount => @subtotal,
-		    :currency => "gbp",
-		    :customer => customer.id
+			:amount => @subtotal,
+			:currency => "gbp",
+			:customer => customer.id
 		)
-    unless charge.paid
-      flash[:alert] = "There has been a problem processing your request"
-      return render root_path
-    end
+		unless charge.paid
+			flash[:alert] = "There has been a problem processing your request"
+			return render root_path
+		end
 
-    @checkout.user = current_user
-    @checkout.stripe_transaction_id = charge.id
-    @checkout.total_price = @subtotal
-    @checkout.sent = false
-    @checkout.save
+		@checkout.user = current_user
+		@checkout.stripe_transaction_id = charge.id
+		@checkout.total_price = @subtotal
+		@checkout.sent = false
+		@checkout.save
 
-    get_cart_magazines.each do |magazine|
-      @checkout.magazines.push(magazine)
-    end
-  end
+		get_cart_magazines.each do |magazine|
+			@checkout.magazines.push(magazine)
+		end
+	end
 end
--- app/models/checkout.rb
+++ app/models/checkout.rb
@@ -1,13 +1,19 @@
 class Checkout < ActiveRecord::Base
 
+	attr_accessor :is_by_post
   attr_accessible :address, :post_code, :city
   belongs_to :user
 
   has_many :checkout_magazines
   has_many :magazines, :through => :checkout_magazines
 
-  validates :address, :presence => true
-  validates :post_code, :presence => true
-  validates :city, :presence => true
+  validates :address, :presence => true, :unless => :magazines_all_ebooks
+  validates :post_code, :presence => true, :unless => :magazines_all_ebooks
+  validates :city, :presence => true, :unless => :magazines_all_ebooks
+
+	private
+	def magazines_all_ebooks
+		!@is_by_post
+	end
 
 end
